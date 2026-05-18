722ed09329242844ee7608b2caa621d9ee04d309
SyntaxAdil <abdurrahmanadil005@gmail.com>
2026-05-18 16:58:52 +0600

    get rooms + searching api integrated done

--- server.js
+++ server.js
@@ -19,32 +19,45 @@ app.post("/rooms", async (req, res) => {
       res.status(400).json({ success: false, message: "Give valid data" });
 
     const newRoom = await roomsCollection().insertOne(body);
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "Room created successfully",
-        data: newRoom,
-      });
+    res.status(200).json({
+      success: true,
+      message: "Room created successfully",
+      data: newRoom,
+    });
   } catch (error) {
     res.status(501).json({ success: false, message: "Internal Server Error" });
   }
 });
 
-// get rooms
+// get rooms with searching
 
 app.get("/rooms", async (req, res) => {
   try {
-    const allRoom = await roomsCollection().find().toArray();
-    res
-      .status(200)
-      .json({
-        success: true,
-        message: "Room fetched successfully",
-        data: allRoom,
-      });
+    const search = req.query.search?.trim();
+    let searchRoom;
+    if (search) {
+      searchRoom = await roomsCollection()
+        .find({
+          roomName: {
+            $regex: search,
+            $options: "i",
+          },
+        })
+        .toArray();
+    } else {
+      searchRoom = await roomsCollection().find().toArray();
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Room fetched successfully",
+      data: searchRoom,
+    });
   } catch (error) {
-    res.status(501).json({ success: false, message: "Internal Server Error" });
+    return res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
   }
 });
 
